d37c1ee2ce9c0002ba6bd265f1a67b37d2d2da98
bienefc <markiskino35@gmail.com>
2024-07-27 19:30:29 +0800

    feat: tambah navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import PageHeader from "./components/PageHeader";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
+    <div>
       <PageHeader />
-    </>
+      <Navbar />
+    </div>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-full bg-[#F6D4C6]">
+      <div className="flex flex-row">
+        <a href="" className="px-4 py-2 text-sm capitalize text-[#b50f17]">
+          home
+        </a>
+        <a href="" className="px-4 py-2 text-sm capitalize text-[#b50f17]">
+          categories
+        </a>
+        <a href="" className="px-4 py-2 text-sm capitalize text-[#b50f17]">
+          pornstars
+        </a>
+        <a href="" className="px-4 py-2 text-sm capitalize text-[#b50f17]">
+          popular videos
+        </a>
+        <a href="" className="px-4 py-2 text-sm capitalize text-[#b50f17]">
+          new videos
+        </a>
+        <a href="" className="px-4 py-2 text-sm capitalize text-[#b50f17]">
+          top rated videos
+        </a>
+        <a href="" className="px-4 py-2 text-sm capitalize text-[#b50f17]">
+          out networks
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
